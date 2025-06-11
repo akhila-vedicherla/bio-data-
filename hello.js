0ec5d488859d0c362474e86a0668054798e6f7d2
akhila-vedicherla <vedicherlaakhila@gmail.com>
2025-06-11 23:55:32 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,27 @@
+document.body.style.backgroundColor="green"
+let title=document.createElement("h1")
+title.innerText="Bio Data"
+title.style.textAlign="center"
+document.body.appendChild(title);
+
+let name=document.createElement("h2")
+name.innerText="Name: Vedicherla Akhila"
+document.body.append(name)
+
+let Qualification=document.createElement("h3")
+Qualification.innerText=" Qualification: Completed BTECH in the Year of 2025"
+document.body.append(Qualification)
+
+let Skills=document.createElement("h3")
+Skills.innerText="Skills: Python,HTML,CSS, JAVASCRIPT,SQL"
+document.body.append(Skills)
+
+let Hobbies=document.createElement("h3")
+Hobbies.innerText="Hobbies: Spending time with family, Gardening Plants,Listening Music"
+document.body.append(Hobbies)
+
+let Archivements=document.createElement("h3")
+Archivements.innerText="Archivements: I have got a Certificate in 24hours Hackthon"
+document.body.append(Archivements)
+
+
